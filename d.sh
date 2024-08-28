435c5d8354158da17e9cf1ff7e532b10781e8f9f
Mohammed Foud <mfoud444@gmail.com>
2024-08-28 09:10:33 +0300

    i commit

--- d.sh
+++ d.sh
@@ -1,39 +1,48 @@
 #!/bin/bash
 
+# Function to handle directory changes and script execution
+run_script_in_directory() {
+  local dir="$1"
+  local script="$2"
+  cd "$dir" || { echo "Failed to change directory to $dir"; exit 1; }
+  bash "$script"
+  cd "$original_dir" || { echo "Failed to return to the original directory"; exit 1; }
+}
+
+# Function to copy the APK file
+copy_apk() {
+  cp code/agriapp/build/app/outputs/flutter-apk/app-release.apk output/app/android/agri-expert.apk
+}
+
+# Function to handle Git operations
+handle_git() {
+  # Uncomment if initializing a new Git repository
+  # git init
+
+  git add .
+  git commit -m "i commit"
+
+  # Uncomment and set the remote repository URL if not already set
+  # git remote add origin https://github.com/agriai444/agri-ai.git
+
+  # Uncomment if renaming the default branch to 'main'
+  # git branch -M main
+
+  git push -u origin main
+}
+
+# Main script execution
+
 # Save the current directory to return to later
 original_dir=$(pwd)
 
-# Change to the admin directory and run the admin script
-cd code/admin-web || { echo "Failed to change directory to code/admin"; exit 1; }
-bash d.sh
 
-# Return to the original directory
-cd "$original_dir" || { echo "Failed to return to the original directory"; exit 1; }
+# run_script_in_directory "code/admin-web" "d.sh"
 
-# Change to the agriapp directory and build the agriapp
-cd code/agriapp || { echo "Failed to change directory to code/agriapp"; exit 1; }
-bash build.sh
+# run_script_in_directory "code/agriapp" "build.sh"
 
-# Return to the original directory
-cd "$original_dir" || { echo "Failed to return to the original directory"; exit 1; }
 
-# Copy the APK file to the output directory
-cp code/agriapp/build/app/outputs/flutter-apk/app-release.apk output/app/android/agri-expert.apk
+# copy_apk
 
-# Uncomment if initializing a new Git repository
-# git init
 
-# Add all changes to Git
-git add .
-
-# Commit changes with a message
-git commit -m "i commit"
-
-# Uncomment and set the remote repository URL if not already set
-# git remote add origin https://github.com/agriai444/agri-ai.git
-
-# Uncomment if renaming the default branch to 'main'
-# git branch -M main
-
-# Push changes to the remote repository
-git push -u origin main
+handle_git
