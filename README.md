7f2a9d8fb1c77163cfdc4d0af5b0af285bba6c0d
Mohammed Foud <mfoud444@gmail.com>
2024-08-28 08:58:57 +0300

    i commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # agri-ai
+
+
+## Download
+
+Run Buttons is a **cross-platform, free and open source** application. You can compile the desktop application yourself or download ready binary files for your platform.
+
+- [Linux amd64](https://github.com/gentee/run-buttons/releases/download/v1.0.0/run-buttons-1.0.0-linux-amd64.zip)
+- [Windows amd64](https://github.com/gentee/run-buttons/releases/download/v1.0.0/run-buttons-1.0.0-windows-amd64.zip)
+- [macOS amd64](https://github.com/gentee/run-buttons/releases/download/v1.0.0/run-buttons-1.0.0-darwin-amd64.zip)
+
+You can download other binary distributions for Linux, macOS, Windows [here](https://github.com/gentee/run-buttons/releases).
+
+## Mobile client
+
+[<img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png' width="250"/>](https://play.google.com/store/apps/details?id=net.gentee.run_buttons_phone&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1)
+
+- **[Run Buttons client for Android](https://github.com/gentee/run_buttons_phone)**
--- d.sh
+++ d.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
-bash code/admin/d.sh
-bash code/agriapp/build.sh
-cp code/agriapp/build/app/outputs/flutter-apk/app-release.apk  output/app/android/agri-expert.apk
+
+# Save the current directory to return to later
+original_dir=$(pwd)
+
+# Change to the admin directory and run the admin script
+cd code/admin-web || { echo "Failed to change directory to code/admin"; exit 1; }
+bash d.sh
+
+# Return to the original directory
+cd "$original_dir" || { echo "Failed to return to the original directory"; exit 1; }
+
+# Change to the agriapp directory and build the agriapp
+cd code/agriapp || { echo "Failed to change directory to code/agriapp"; exit 1; }
+bash build.sh
+
+# Return to the original directory
+cd "$original_dir" || { echo "Failed to return to the original directory"; exit 1; }
+
+# Copy the APK file to the output directory
+cp code/agriapp/build/app/outputs/flutter-apk/app-release.apk output/app/android/agri-expert.apk
+
+# Uncomment if initializing a new Git repository
 # git init
+
+# Add all changes to Git
 git add .
+
+# Commit changes with a message
 git commit -m "i commit"
+
+# Uncomment and set the remote repository URL if not already set
 # git remote add origin https://github.com/agriai444/agri-ai.git
+
+# Uncomment if renaming the default branch to 'main'
 # git branch -M main
-git push -u origin main 
+
+# Push changes to the remote repository
+git push -u origin main
